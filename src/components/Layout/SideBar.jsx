77fd3c12dccb3dfdec64a2ef760d638553c9cbf3
SonnX <SonnX_18@outlook.com>
2026-07-14 11:36:27 -0600

    Datalle logo

--- src/components/Layout/SideBar.jsx
+++ src/components/Layout/SideBar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/YINLOGO.png";
+import logo from "../../assets/YINLOGO.PNG";
 
 function SideBar({ onLogout }) {
   return (
